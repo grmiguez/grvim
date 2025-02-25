67ed19178286c159336fb7acea26faf395620159
guidoMiguez <grmiguez@gmail.com>
2025-02-25 14:16:20 -0300

    lsp-zero update

--- lua/setup/lsp-zero.lua
+++ lua/setup/lsp-zero.lua
@@ -1,10 +1,20 @@
 local lsp = require('lsp-zero').preset({})
+local cmp = require('cmp')
 
 lsp.on_attach(function(client, bufnr)
-  lsp.default_keymaps({buffer = bufnr})
+    lsp.default_keymaps({ buffer = bufnr })
 end)
 
 -- (Optional) Configure lua language server for neovim
 require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
 
 lsp.setup()
+cmp.setup({
+    sources = cmp.config.sources({
+        { name = 'nvim_lsp' },
+        { name = 'vsnip' },   -- For vsnip users.
+        { name = 'luasnip' }, -- For luasnip users.
+        -- { name = 'ultisnips' }, -- For ultisnips users.
+        -- { name = 'snippy' }, -- For snippy users.
+    }),
+})
